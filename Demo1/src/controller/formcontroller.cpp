4fa51d2db42c2faf6ef5be86cc1cec4e37cc2e40
aylhttd <aylhttd@gmail.com>
2021-08-16 12:22:38 +0300

    formcontrooler update

--- Demo1/src/controller/formcontroller.cpp
+++ Demo1/src/controller/formcontroller.cpp
@@ -4,34 +4,45 @@
 */
 
 #include "formcontroller.h"
+#include <iostream>
 
 FormController::FormController()
 {}
 
 void FormController::service(HttpRequest& request, HttpResponse& response)
 {
+    response.setHeader("Content-Type", "text/html; charset=utf-8");
 
-    response.setHeader("Content-Type", "text/html; charset=UTF-8");
 
     if (request.getParameter("action")=="show")
     {
         response.write("<html><body>");
-        response.write("Name = ");
-        response.write(request.getParameter("name"));
-        response.write("<br>City = ");
-        response.write(request.getParameter("city"));
-        response.write("</body></html>",true);
+        response.write("<form method=\"post\">");
+        response.write("  <input type=\"hidden\" name=\"action\" value=\"show\">");
+        response.write("<textarea name=\"name\" cols=\"100\" rows=\"10\" placeholder=\"input text\"></textarea>");
+
+        response.write("<html><body>");
+        response.write("<form method=\"post\">");
+        response.write("<input type=\"hidden\" name=\"action\" value=\"show\">");
+        response.write("<textarea name=\"name\" cols=\"100\" rows=\"10\" readonly=\"readonly\">" + QString(request.getParameter("name")).toUtf8() + "</textarea>");
+
+        /*response.write("<br>OUTPUT = ");
+        response.write(QString(request.getParameter("name")).toUtf8());
+        response.write("</body></html>",true);*/
     }
     else
     {
         response.write("<html><body>");
         response.write("<form method=\"post\">");
-        response.write("  <input type=\"hidden\" name=\"action\" value=\"show\">");
-        response.write("  Name: <input type=\"text\" name=\"name\"><br>");
-        response.write("  City: <input type=\"text\" name=\"city\"><br>");
-        response.write("  <input type=\"submit\">");
+        response.write("<input type=\"hidden\" name=\"action\" value=\"show\">");
+
+        response.write("<textarea name=\"name\" cols=\"100\" rows=\"10\" placeholder=\"input text\" autofocus></textarea>");
+        response.write("<textarea name=\"city\" padding=\"100\" cols=\"100\" rows=\"10\" disabled=\"disabled\" placeholder=\"output text\"></textarea>");
+
+        response.write("<input type=\"submit\"> ");
         response.write("</form>");
         response.write("</body></html>",true);
+
     }
 }
 
